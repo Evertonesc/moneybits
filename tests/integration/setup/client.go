0974365fe11c6711b6f00ee18c049177f02665ac
esc <evertonsouzaesc@gmail.com>
2025-04-13 02:59:25 -0300

    test(wip): tweak integration test setup

--- tests/integration/setup/client.go
+++ tests/integration/setup/client.go
@@ -30,7 +30,7 @@ func NewTestRestClient(server *http.Server) *TestClient {
 	}
 }
 
-func (c *TestClient) Post(path string, body interface{}, response interface{}) (*http.Response, error) {
+func (c *TestClient) Post(path string, body interface{}) (*http.Response, error) {
 	jsonBody, err := json.Marshal(body)
 	if err != nil {
 		return nil, fmt.Errorf("failed to marshal request body: %v", err)
@@ -41,37 +41,13 @@ func (c *TestClient) Post(path string, body interface{}, response interface{}) (
 		return nil, fmt.Errorf("failed to make request: %v", err)
 	}
 
-	if response != nil {
-		defer resp.Body.Close()
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, fmt.Errorf("failed to read response body: %v", err)
-		}
-
-		if err := json.Unmarshal(body, response); err != nil {
-			return nil, fmt.Errorf("failed to unmarshal response: %v", err)
-		}
-	}
-
 	return resp, nil
 }
 
-func (c *TestClient) Get(path string, response interface{}) (*http.Response, error) {
+func (c *TestClient) Get(path string) (*http.Response, error) {
 	resp, err := http.Get(c.Server.URL + path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to make request: %v", err)
-	}
-
-	if response != nil {
-		defer resp.Body.Close()
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, fmt.Errorf("failed to read response body: %v", err)
-		}
-
-		if err := json.Unmarshal(body, response); err != nil {
-			return nil, fmt.Errorf("failed to unmarshal response: %v", err)
-		}
 	}
 
 	return resp, nil
