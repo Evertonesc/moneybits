536f3b9afbebe1323fa4d81ddd90f8a7684e2cde
esc <evertonsouzaesc@gmail.com>
2025-04-13 02:57:43 -0300

    feat: setup the stocks module to app container

---
+++ core/stocks/module.go
@@ -0,0 +1,35 @@
+package stocks
+
+import (
+	"moneybits/adapters/brapi"
+	"moneybits/core/stocks/api"
+	"moneybits/core/stocks/usecases"
+
+	"github.com/labstack/echo/v4"
+)
+
+type AppContainer interface {
+	HTTPRouter() *echo.Router
+}
+
+type StocksModule struct {
+	app AppContainer
+}
+
+func NewSocksModule(app AppContainer) *StocksModule {
+	return &StocksModule{
+		app: app,
+	}
+}
+func (m *StocksModule) Name() string {
+	return "stocks"
+}
+func (m *StocksModule) Start() error {
+	brapiAdapter := brapi.NewBrapiAdapter()
+	fetchTickerQuoteUC := usecases.NewFetchTickerQuoteUC(brapiAdapter)
+	fetchTickerQuoteHandler := api.NewFetchTickerQuoteHandler(fetchTickerQuoteUC)
+
+	api.RegisterStocksRouter(m.app.HTTPRouter(), fetchTickerQuoteHandler)
+
+	return nil
+}
